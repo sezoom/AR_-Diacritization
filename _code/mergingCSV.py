f35eca95a20a6e2238cc18fd269c52d88d21908f
Hamad ALSHEHHI <sezoom@gmail.com>
2025-04-03 15:44:07 +0400

    computing statistics for the different datasets

--- _code/mergingCSV.py
+++ _code/mergingCSV.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 ## to merge multiple csv  files with same structure
-directory = "output/test"
+directory = "../datasets/arzEn/"
 
 csv_files = [file for file in os.listdir(directory) if file.endswith(".csv")]
 
@@ -11,6 +11,6 @@ df_list = [pd.read_csv(os.path.join(directory, file)) for file in csv_files]
 merged_df = pd.concat(df_list, ignore_index=True)
 
 
-merged_df.to_csv("output/test_cs_chatpt4o.csv", index=False)
+merged_df.to_csv("../datasets/arzEn.csv", index=False)
 
 print(merged_df.head())
